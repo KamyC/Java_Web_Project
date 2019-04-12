98059f3395d881278ed74bb9bc65dd78c75db26f
KamyC <caojinghan123@hotmail.com>
2019-04-12 00:55:21 -0700

    delete

--- Login Web/src/dao/IUserDao.java
+++
@@ -1,8 +0,0 @@
-package dao;
-
-import model.User;
-
-public interface IUserDao {
-    public void insertUser(User user);
-    public User findUser(String name,String password);
-}
--- Login Web/src/utils/UserException.java
+++
@@ -1,7 +0,0 @@
-package utils;
-
-public class UserException extends Exception {
-    public UserException(String message) {
-        super(message);
-    }
-}
